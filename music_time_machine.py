8208355ce318a2262caaf3c229715c08a778ec5c
Matthew Anderson <anderson.c.matt@gmail.com>
2022-02-07 12:39:37 -0600

    Scrape top 100 songs into list

--- music_time_machine.py
+++ music_time_machine.py
@@ -2,17 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 import argparse
 
+SELECTOR = "li.lrv-u-flex-grow-1:first-child"
+URL = "https://www.billboard.com/charts/hot-100"
+
 parser = argparse.ArgumentParser(description="Create a playlist of the Billboard Hot 100 from any given date")
 parser.add_argument("date", type=str, help="YYYY-MM-DD - Date to pick music from")
 
 args = parser.parse_args()
 date = args.date
 
-url = f"https://www.billboard.com/charts/hot-100/{date}"
-
-response = requests.get(url)
+response = requests.get(f"{URL}/{date}")
 response.raise_for_status()
 
 soup = BeautifulSoup(response.text, features="html.parser")
+
+top_songs = [
+    {
+        "Song": tag.select_one("h3").getText().strip(),
+        "Artist": tag.select_one("span").getText().strip()
+    }
+    for tag in soup.select(SELECTOR)
+]
 
 
