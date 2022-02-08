a75e794118e4fe9664c0408a9492afb9fb0c28b0
Matthew Anderson <anderson.c.matt@gmail.com>
2022-02-08 15:15:23 -0600

    Changed from using spotipy to using requests-oauthlib to authorize

--- music_time_machine.py
+++ music_time_machine.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import requests_oauthlib
 import argparse
-import spotipy
 import os
 import dotenv
 
@@ -12,6 +12,8 @@ URL = "https://www.billboard.com/charts/hot-100"
 dotenv.load_dotenv("./.env")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+SCOPE = ["playlist-modify-public"]
 
 # Argument Parsing
 parser = argparse.ArgumentParser(description="Create a playlist of the Billboard Hot 100 from any given date")
@@ -35,11 +37,18 @@ top_songs = [
 ]
 
 # Authenticating with spotify
-sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(client_id=CLIENT_ID,
-                                                       client_secret=CLIENT_SECRET,
-                                                       redirect_uri="https://example.com",
-                                                       scope="playlist-modify-public"))
-user_data = sp.current_user()
+oauth = requests_oauthlib.OAuth2Session(client_id=CLIENT_ID,
+                                        redirect_uri=REDIRECT_URI,
+                                        scope=SCOPE)
 
+auth_url, state = oauth.authorization_url(url="https://accounts.spotify.com/authorize")
 
+auth_response = input(f"Please go to {auth_url} and authorize access.\n"
+      f"Enter the full callback URL")
+
+token = oauth.fetch_token(token_url="https://accounts.spotify.com/api/token",
+                          authorization_response=auth_response,
+                          client_secret=CLIENT_SECRET)
+
+user_data = oauth.get("https://api.spotify.com/v1/me").json()
 
