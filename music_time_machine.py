51110220ab76d0f65f3256eac350ae930e33d48d
Matthew Anderson <anderson.c.matt@gmail.com>
2022-02-08 16:22:06 -0600

    Create playlist

--- music_time_machine.py
+++ music_time_machine.py
@@ -14,14 +14,17 @@ dotenv.load_dotenv("./.env")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
-SCOPE = ["playlist-modify-public"]
+SCOPE = ["playlist-modify-public", "playlist-modify-private"]
 
 # Argument Parsing
 parser = argparse.ArgumentParser(description="Create a playlist of the Billboard Hot 100 from any given date")
 parser.add_argument("date", type=str, help="YYYY-MM-DD - Date to pick music from")
+parser.add_argument("is_public", type=bool, help="True - To create a public playlist\n"
+                                                 "False - To create a private playlist")
 
 args = parser.parse_args()
 date = args.date
+is_public = args.is_public
 
 # Web Scraping the top 100 songs
 response = requests.get(f"{URL}/{date}")
@@ -52,7 +55,6 @@ token = oauth.fetch_token(token_url="https://accounts.spotify.com/api/token",
                           client_secret=CLIENT_SECRET)
 
 # Get songs
-user = oauth.get(f"{API_URL}/me").json()
 
 song_ids = []
 for song in top_songs:
@@ -66,5 +68,14 @@ for song in top_songs:
     if items:
         song_ids.append(items[0]["id"])
 
+
+# Create playlist
+user = oauth.get(f"{API_URL}/me").json()
+json = {
+    "name": f"Top 100 songs of {date}",
+    "public": is_public,
+    "description": f"Top 100 songs of {date}"
+}
+response = oauth.post(f"{API_URL}/users/{user['id']}/playlists", json=json)
 
 
